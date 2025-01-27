e167cab4c5b297cfd621d139c3841d175f3d10d2
wblaiklock <wblaiklock@gmail.com>
2025-01-27 13:21:14 -0500

    Changed api call to HTTPS

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   const getMovie = async (searchTerm) => {
     try{
-      const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
+      const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
       const data = await response.json()
 
       setMovie(data);
